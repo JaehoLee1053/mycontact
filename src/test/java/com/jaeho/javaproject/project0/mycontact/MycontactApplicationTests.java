d773d2f6a43de083c4a836ae46e761d1ce3a3598
JaehoLee1053 <dlwogh1053@gmail.com>
2021-05-14 02:15:51 +0900

    Title: Make New Project
    Body
    Push for test.
    
    Resolves : None
    
    See also : None

---
+++ src/test/java/com/jaeho/javaproject/project0/mycontact/MycontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeho.javaproject.project0.mycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
