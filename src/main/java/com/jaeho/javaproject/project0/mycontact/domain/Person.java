1cbdc0118c25cac06d7e127e826ed67afc96a9f3
JaehoLee1053 <dlwogh1053@gmail.com>
2021-05-18 02:42:38 +0900

    Title: Lombok
    Body
    Apply @Getter @Setter @ToString annotation.
    Hide entity using @ToString.Exclude annotation.
    
    Apply @Constructor @EqualsAndHashCode @Data annotation.
    @EqualsAndHashCode makes them get same hashcode if the have same entity values.
    @Data involves @Getter, @Setter, @RequiredArgsConstructor, @ToString, and @EqualsAndHashCode.
    
    Resolves : None
    
    See also : None

--- src/main/java/com/jaeho/javaproject/project0/mycontact/domain/Person.java
+++ src/main/java/com/jaeho/javaproject/project0/mycontact/domain/Person.java
@@ -1,31 +1,42 @@
 package com.jaeho.javaproject.project0.mycontact.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.sun.istack.NotNull;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Person {
 
     @Id
     @GeneratedValue // 자동으로 생성되게
     private long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+    private String hobby;
+
+    @NonNull
+    private String bloodType;
+
+    private String address;
+
+    private LocalDate birthday;
+
+    private String job;
+
+    @ToString.Exclude
+    private String phoneNumber;
+
 }
--- src/test/java/com/jaeho/javaproject/project0/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/jaeho/javaproject/project0/mycontact/repository/PersonRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,15 +26,39 @@ class PersonRepositoryTest {
         Person person = new Person();
         person.setName("martin");
         person.setAge(10);
+        person.setBloodType("A");
 
         personRepository.save(person);
 
-//        System.out.println(personRepository.findAll());
+        System.out.println(personRepository.findAll());
         List<Person> people = personRepository.findAll();
 
         assertEquals(1, people.size());
         assertEquals("martin", people.get(0).getName());
         assertEquals(10, people.get(0).getAge());
+        assertEquals("A", people.get(0).getBloodType());
+
+    }
+
+    @Test
+    void contructor() {
+        Person person = new Person("martin", 10, "A");
+    }
+
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("martin", 10, "A");
+        Person person2 = new Person("martin", 10, "A");
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
 
     }
 
